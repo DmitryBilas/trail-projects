c965a0026595078787382288630c43d70639bb9f
DmitryBilas <dmitry.bilas@gmail.com>
2021-11-02 11:30:34 +0300

    added my projects

---
+++ python-project/app.py
@@ -0,0 +1,5 @@
+import time
+
+for x in range(1,5):
+	print('Hello world!')
+	time.sleep(1)
---
+++ web-db/app.py
@@ -0,0 +1,15 @@
+from storage import MongodbService
+from uuid import uuid4 
+
+storage = MongodbService.get_instance()
+
+for _ in range(5):
+	dto = {
+	"_id": str(uuid4()),
+	"payload": str(uuid4()),
+	
+	}
+	storage.save_data(dto)
+
+for data in storage.get_data():
+	print(data)
